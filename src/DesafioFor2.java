d77a222c6e2756791949295ee8d8b92afccab01d
uislancruz <uislansk8@hotmail.com>
2022-09-26 21:23:39 -0300

    Desafio For 2

---
+++ src/DesafioFor2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DesafioFor2 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite um número positivo inteiro: ");
+        int numero = entrada.nextInt();
+
+        boolean numeroPrimo = true;
+
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                numeroPrimo = false;
+                break;
+            }
+        }
+
+        if (numeroPrimo && numero > 1) {
+            System.out.printf("%d é um número primo", numero);
+        } else {
+            System.out.printf("%d não é um número primo", numero);
+        }
+    }
+    }
+
