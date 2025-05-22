dc5f535fdf1a06da578440b48e284ec6bb02f9ec
Uislan Cruz <uislansk8@hotmail.com>
2025-05-21 21:47:42 -0300

    incluindo nova classe do projeto carrinho de compra - Clean Code

--- src/CarrinhoDeCompra.java
+++ src/CarrinhoDeCompra.java
@@ -1,4 +1,25 @@
 public class CarrinhoDeCompra {
+
+    static final double TAXA_SERVICO_PADRAO = 0.10;
+
     int quantidadeItens;
     Produto produto;
+
+    void realizarCompra(String numeroCarta){
+        double valorTotal = calcularValorTotal();
+
+        boolean pagamentoAutorizado = GatewayPagamento.autorizarPagamento(numeroCarta, valorTotal);
+
+        if (pagamentoAutorizado){
+            System.out.println("Recibo de Pagamento");
+            System.out.printf("Valor total: R$%2.f%n", valorTotal);
+        }
+    }
+
+    double calcularValorTotal() {
+        double subtotal = produto.precoUnitario * quantidadeItens;
+        double taxaServico = subtotal * TAXA_SERVICO_PADRAO;
+        double valorTotal = subtotal + taxaServico;
+        return valorTotal;
+    }
 }
